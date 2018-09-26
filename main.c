fc84540fcf6fd31d600f4d7641dc542b7d5dd79a
Njut <43410740+NathanNjut@users.noreply.github.com>
2018-09-26 13:56:28 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,20 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include"LIST.h"
+#include"QUEUE.h"
+#include "LIST.h"
+/*
+struct lexem_t;
+typedef struct lexem_t *lexem;
+
+int lexem_print( void*ptr ) {
+  lexem lex = ptr;
+  printf( "%d", lex->nline );
+}
+*/
 
 
 main(){
 int a=2,b=4,c=0;
-char s[512]="salut";
-
-printf("%s\n", s);
-
 LIST l=create_list();
-l=head_insert(l,&a);
-l=head_insert(l,&b);
-l=head_insert(l,&c);
-//visualize(l,"%d");
-void* p =l->pdata;
-printf("%d\n",*p);
 
+l=head_insert_list(l,&c);
+l=head_insert_list(l,&b);
+l=head_insert_list(l,&b);
+
+visualize_int_list(l);
+/*
+l=head_delete_list(l);
+l=head_delete_list(l);
+l=head_delete_list(l);
+*/
+free_list(&l);
+visualize_int_list(l);
+
+l=head_insert_list(l,&b);
+l=head_insert_list(l,&c);
+free_list(&l);
+visualize_int_list(l);
 }
